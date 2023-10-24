7a0ce97bfdee6142b1e836fa53daf6370bef7e0b
hothugiang <21020309@vnu.edu.vn>
2023-10-24 23:33:50 +0700

    FE

---
+++ scr/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style = {{ alignItems: "center" , marginTop: 50 }}>
+      <Text>Home Screen</Text>
+    </View>
+
+  );
+}
