ed10660a7d589fe53191b910df1269f955e47d6a
ngocminh1605 <100121817+ngocminh1605@users.noreply.github.com>
2023-12-19 21:03:49 +0700

    ho

--- scr/constants/Axios.js
+++ scr/constants/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://192.168.1.4:3000';
+const baseURL = 'http://192.168.204.82:3000';
 const axiosInstance = axios.create({
   baseURL: baseURL,
   timeout: 10000,
